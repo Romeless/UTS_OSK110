7406b1f555c5c1886da1cc6adb95ba2a409a1a07
Rama Lesmana <ramalesmana.1999@gmail.com>
2019-05-01 13:36:01 +0700

    add

--- artificial.py
+++ artificial.py
@@ -14,4 +14,13 @@ if __name__ == "__init__":
             
             im = Image.open(filename)
             
+            rot90 = im.transpose(Image.ROTATE_90)
+            rot180 = im.transpose(Image.ROTATE_180)
+            rot270 = im.transpose(Image.ROTATE_270)
+
+            
+            
+            save_image(rot90, filename, info="rot90")
+            save_image(rot180, filename, info="rot180")
+            save_image(rot270, filename, info="rot270")
             
